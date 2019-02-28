9cec047bd42a4e83b89af6a02526afaa78cb3c6c
Nick Tailor <nikesh.tailor2@gmail.com>
2019-02-28 17:09:33 +0000

    FirstBuild
    RogueRun, with all assets included.
    
    Use this solution and build upon it from now on.

---
+++ RogueRun/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+
+}
