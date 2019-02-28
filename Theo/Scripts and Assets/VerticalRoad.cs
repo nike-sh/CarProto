6381d20f7d9ed6287ba2a21b5e12367691f07329
RudeHolan <47670602+RudeHolan@users.noreply.github.com>
2019-02-28 16:22:02 +0200

    Add files via upload

---
+++ Theo/Scripts and Assets/VerticalRoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalRoad : MonoBehaviour
+{
+    public GameObject Car;
+    public GameObject CarCollider;
+
+   
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Car") || collision.CompareTag("VerticalRoads"))
+        {
+            Car.SendMessage("OnVerticalRoad");
+            CarCollider.SendMessage("OnVerRoad");
+            Debug.Log("OnVerticalRoad");
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Car") || collision.CompareTag("VerticalRoads"))
+        {
+            Car.SendMessage("OffVerticalRoad");
+            CarCollider.SendMessage("OffVerRoad"); 
+            Debug.Log("OffVerticalRoad");
+        }
+    }
+
+}
