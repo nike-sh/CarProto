56310aa19b1d7731069dd58fb0497a3eee4b8548
stevewatkins22 <steve.watkins22@gmail.com>
2019-03-01 13:54:04 +0000

    Basic Movement for enemy & player
    EnemyMovement script has basic follow & patrol functionallity, will incorperate the enemyattack behaviour into the same script

---
+++ assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float speed;
+    public float followSpeed;
+    private float waitTime;
+    public float startWaitTime;
+    private Transform target;
+    public float followDistance;
+
+    public Transform[] moveSpots;
+    private int destPoints = 0;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+        GotoNextPoint();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void GotoNextPoint()
+    {
+        if (moveSpots.Length == 0)
+        return;
+   
+        agent.destination = moveSpots[destPoints].position;
+
+        destPoints = (destPoints + 1) % moveSpots.Length;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) < followDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots[destPoints].position, speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, moveSpots[destPoints].position) < 0.2f)
+            {
+                if(waitTime <= 0)
+                {
+                    GotoNextPoint();
+                    waitTime = startWaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
+    }
+}
---
+++ assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        player.AddForce(movement * speed);
+    }
+}
