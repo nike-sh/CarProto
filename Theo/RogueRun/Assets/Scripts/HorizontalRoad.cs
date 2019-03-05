d8b70eb4bff54c9429fad994f633184ce82684a5
RudeHolan <bbp2443@gmail.com>
2019-03-05 16:24:35 +0000

    Update

---
+++ Theo/RogueRun/Assets/Scripts/HorizontalRoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalRoad : MonoBehaviour
+{
+    public GameObject Car;
+    public GameObject CarCollider;
+   
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Car") || collision.CompareTag("HorizontalRoads"))
+        {
+            Car.SendMessage("OnHorizontalRoad");
+            CarCollider.SendMessage("OnHorRoad");
+            Debug.Log("OnHorizontal Road");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Car") || collision.CompareTag("HorizontalRoads"))
+        {
+            Car.SendMessage("OffHorizontalRoad");
+            CarCollider.SendMessage("OffHorRoad");
+            Debug.Log("OffHorizontalRoad");
+        }
+    }
+
+
+
+}
