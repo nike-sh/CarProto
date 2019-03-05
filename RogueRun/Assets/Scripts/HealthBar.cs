e05a6d8ad70f055b995b484874bc5c1cbfc9f49e
Nick Tailor <nikesh.tailor2@gmail.com>
2019-03-05 19:18:54 +0000

    Started HealthBar
    Need some help with healthbar sprites

---
+++ RogueRun/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    private HealthSystem healthSystem;
+
+    public void Setup(HealthSystem healthSystem)
+    {
+        healthSystem.OnHealthChange += HealthSystem_OnHealthChanged; ;
+    }
+	
+    private void HealthSystem_OnHealthChanged(object sender, System.EventArgs e)
+    {
+        transform.Find("Bar").localScale = new Vector3(healthSystem.GetHealthPercent(), 1);
+    }
+	
+}
