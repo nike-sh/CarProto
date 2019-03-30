f1c40092daf9a7a6684d0c99c18df46bec3d8f3f
Nick Tailor <nikesh.tailor2@gmail.com>
2019-03-30 16:01:47 +0000

    Added Menu and audio

---
+++ RogueRun/Assets/Scripts/Road Scripts/VerticalRoadsRightLanes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalRoadsRightLanes : MonoBehaviour
+{
+    private GameObject Car;
+
+    void Start()
+    {
+        Car = GameObject.FindWithTag("Car");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CarCollider") || collision.CompareTag("VerticalRoadsRight"))
+        {
+            Car.SendMessage("OnVerticalRoadRight");
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CarCollider") || collision.CompareTag("VerticalRoadsRight"))
+        {
+            Car.SendMessage("OffVerticalRoadRight");
+        }
+    }
+
+}
---
+++ RogueRun/Assets/Scripts/Spike Scripts/SpikesDamage.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikesDamage : MonoBehaviour {
+    [SerializeField]
+    GameObject HealthBar;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        HealthBar.SendMessage("SpikesDamage");
+    }
+
+}
