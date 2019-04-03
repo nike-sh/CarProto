901ab52649af6138f11ede004ea482c2a4509e69
RudeHolan <47670602+RudeHolan@users.noreply.github.com>
2019-04-03 18:57:57 +0300

    try this

---
+++ Try This/Assets/Scripts/Road Scripts/VerticalRoadsLeftLanes.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalRoadsLeftLanes : MonoBehaviour
+{
+    private GameObject Car;
+
+    void Start()
+    {
+        Car = GameObject.FindWithTag("Car");
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CarCollider") || collision.CompareTag("VerticalRoadsLeft"))
+        {
+            Car.SendMessage("OnVerticalRoadLeft");
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("CarCollider") || collision.CompareTag("VerticalRoadsLeft"))
+        {
+            Car.SendMessage("OffVerticalRoadLeft");
+        }
+    }
+
+}
